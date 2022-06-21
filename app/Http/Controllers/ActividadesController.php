09f421c3b5b3187098481777c3c5d4aa0e39aae5
Rafael Najarro <rafaelnajarro10@gmail.com>
2022-06-21 13:38:29 -0600

    actividad update

--- app/Http/Controllers/ActividadesController.php
+++ app/Http/Controllers/ActividadesController.php
@@ -25,12 +25,12 @@ class ActividadesController extends Controller
     public function show($id)
     {
         $actividades = Actividades::find($id);
-        $usuarios = User::all();
+        $coordinadores = User::all();
         $lugares = Lugares::all();
         $organizadores = Lugares::all();
         $estados = EstadosActividades::all();
 
-        return view('show-actividad', ['actividades' => $actividades, 'usuarios' => $usuarios, 'lugares' => $lugares, 'organizadores' => $organizadores, 'estados' => $estados]);
+        return view('show-actividad', ['actividades' => $actividades, 'coordinadores' => $coordinadores, 'lugares' => $lugares, 'organizadores' => $organizadores, 'estados' => $estados]);
     }
 
     public function store(Request $request)
@@ -73,6 +73,37 @@ class ActividadesController extends Controller
 
     public function update($id, Request $request)
     {
+        $request->validate([
+            'id_organizador' => 'required',
+            'id_coordinador' => 'required',
+            'id_lugar' => 'required',
+            'id_estado' => 'required',
+            'nombre_actividad' => 'required',
+            'fecha_inicio' => 'required',
+            'fecha_finalizacion' => 'required',
+            'hora_inicio' => 'required',
+            'hora_finalizacion' => 'required',
+            'objetivo' => 'required',
+            'observaciones' => 'required'
+        ]);
+
+        $actividad = Actividades::find($id);
+
+        $actividad->id_organizador = $request->id_organizador;
+        $actividad->id_lugar = $request->id_lugar;
+        $actividad->id_coordinador = $request->id_coordinador;
+        $actividad->nombre_actividad = $request->nombre_actividad;
+        $actividad->fecha_inicio = $request->fecha_inicio;
+        $actividad->fecha_finalizacion = $request->fecha_finalizacion;
+        $actividad->hora_inicio = $request->hora_inicio;
+        $actividad->hora_finalizacion = $request->hora_finalizacion;
+        $actividad->objetivo = $request->objetivo;
+        $actividad->observaciones = $request->observaciones;
+        $actividad->id_estado = $request->id_estado;
+
+        $actividad->save();
+
+        return redirect()->route('actividades.index')->with('success', 'Actividad actualizada correctamente');
     }
 
     public function destroy($id)
--- resources/views/show-actividad.blade.php
+++ resources/views/show-actividad.blade.php
@@ -4,24 +4,26 @@
 
 <h3 class="my-4">Modificar usuario</h3>
 
-<a class="btn btn-outline-secondary mb-4" href="{{ route('users.index')}}">Regresar</a>
+<a class="btn btn-outline-secondary mb-4" href="{{ route('actividades.index')}}">Regresar</a>
 
 @include('layouts.mensajesUsers')
-<form action="{{ route('actividades.update', ['actividade' => $actividad->id]) }}" method="POST">
+<form action="{{ route('actividades.update', ['actividade' => $actividades->id]) }}" method="POST">
     @csrf
+    @method('PATCH')
     <div class="mb-3">
         <label for="id_organizador" class="col-form-label">Organizador del evento:</label>
         <select id="id_organizador" class="form-select" name="id_organizador">
             @foreach ($organizadores as $organizador )
-            <option @selected($organizador->id_organizador == $organizador->id ) value="{{$organizador->id}}">{{$organizador->nombre}}</option> @endforeach
+            <option @selected($actividades->id_organizador == $organizador->id ) value="{{$organizador->id}}">{{$organizador->nombre}}</option>
+            @endforeach
         </select>
     </div>
 
     <div class="mb-3">
         <label for="id_coordinador" class="col-form-label">Coordinador / Asistente:</label>
         <select id="id_coordinador" class="form-select" name="id_coordinador">
-            @foreach ($usuarios as $usuario )
-            <option value="{{$usuario->id}}">{{$usuario->nombres}}</option>
+            @foreach ($coordinadores as $coordinador )
+            <option @selected($actividades->id_coordinador == $coordinador->id ) value="{{$coordinador->id}}">{{$coordinador->nombres}}</option>
             @endforeach
         </select>
     </div>
@@ -30,51 +32,51 @@
         <label for="id_lugar" class="col-form-label">Lugar:</label>
         <select id="id_lugar" class="form-select" name="id_lugar">
             @foreach ($lugares as $lugar )
-            <option value="{{$lugar->id}}">{{$lugar->nombre}}</option>
+            <option @selected($actividades->id_lugar == $lugar->id ) value="{{$lugar->id}}">{{$lugar->nombre}}</option>
             @endforeach
         </select>
     </div>
 
     <div class="mb-3">
         <label for="nombre_actividad" class="col-form-label">Nombre de actividad:</label>
-        <input type="text" class="form-control" name="nombre_actividad" id="nombre_actividad">
+        <input type="text" class="form-control" name="nombre_actividad" id="nombre_actividad" value="{{$actividades->nombre_actividad}}">
     </div>
 
     <div class="mb-3">
         <label for="fecha_inicio" class="col-form-label">Fecha inicio:</label>
-        <input type="date" class="form-control" name="fecha_inicio" id="fecha_inicio">
+        <input type="date" class="form-control" name="fecha_inicio" id="fecha_inicio" value="{{$actividades->fecha_inicio}}">
     </div>
 
     <div class="mb-3">
         <label for="fecha_finalizacion" class="col-form-label">Fecha finalizacion:</label>
-        <input type="date" class="form-control" name="fecha_finalizacion" id="fecha_finalizacion">
+        <input type="date" class="form-control" name="fecha_finalizacion" id="fecha_finalizacion" value="{{$actividades->fecha_finalizacion}}">
     </div>
 
     <div class="mb-3">
         <label for="hora_inicio" class="col-form-label">Hora inicio:</label>
-        <input type="time" class="form-control" name="hora_inicio" id="hora_inicio">
+        <input type="time" class="form-control" name="hora_inicio" id="hora_inicio" value="{{$actividades->hora_inicio}}">
     </div>
 
     <div class="mb-3">
         <label for="hora_finalizacion" class="col-form-label">Hora finalizacion:</label>
-        <input type="time" class="form-control" name="hora_finalizacion" id="hora_finalizacion">
+        <input type="time" class="form-control" name="hora_finalizacion" id="hora_finalizacion" value="{{$actividades->hora_finalizacion}}">
     </div>
 
     <div class="mb-3">
         <label for="objetivo" class="col-form-label">Objetivo:</label>
-        <input type="text" class="form-control" name="objetivo" id="objetivo">
+        <input type="text" class="form-control" name="objetivo" id="objetivo" value="{{$actividades->objetivo}}">
     </div>
 
     <div class="mb-3">
         <label for="observaciones" class="col-form-label">Observaciones:</label>
-        <input type="text" class="form-control" name="observaciones" id="observaciones">
+        <input type="text" class="form-control" name="observaciones" id="observaciones" value="{{$actividades->observaciones}}">
     </div>
 
     <div class="mb-3">
         <label for="id_estado" class="col-form-label">Estado:</label>
         <select id="id_estado" class="form-select" name="id_estado">
             @foreach ($estados as $estado )
-            <option value="{{$estado->id}}">{{$estado->tipo_estado}}</option>
+            <option @selected($actividades->id_estado == $estado->id ) value="{{$estado->id}}">{{$estado->tipo_estado}}</option>
             @endforeach
         </select>
     </div>
