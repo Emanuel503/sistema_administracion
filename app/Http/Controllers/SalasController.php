522e69a14c1a358caec3d253c9457545abe08100
Emanuel Molina <emanueljosemolina@hotmail.es>
2022-06-21 09:45:42 -0600

    CRUD Salas

--- app/Http/Controllers/SalasController.php
+++ app/Http/Controllers/SalasController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Salas;
 use Illuminate\Http\Request;
 
 class SalasController extends Controller
 {
     public function index(){
-        
+        $salas = Salas::all();
+        return view('solicitudes-salas',['salas' => $salas]);
     }
 }
