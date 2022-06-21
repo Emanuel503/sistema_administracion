88534e2f5afddc1171ec0b8100791b30a9a3b0c5
Rafael Najarro <rafaelnajarro10@gmail.com>
2022-06-21 09:45:34 -0600

    app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,32 +8,33 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="http://localhost/sistema_disam/resources/css/menu.css" rel="stylesheet">
 </head>
+
 <body>
 
     @yield('menu')
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
- 	        <a class="navbar-brand" href="{{url('/home')}}">Sistema de administracion</a>
+            <a class="navbar-brand" href="{{url('/home')}}">Sistema de administracion</a>
 
-             @guest
-             @if (Route::has('login'))
-             @endif
-             @else
-             
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main_nav"  aria-expanded="false" aria-label="Toggle navigation">
+            @guest
+            @if (Route::has('login'))
+            @endif
+            @else
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main_nav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            
+
             <div class="collapse navbar-collapse" id="main_nav">
                 <ul class="navbar-nav">
                     <li class="nav-item active"> <a class="nav-link" href="{{url('/home')}}">Agenda</a> </li>
                     @if ( Auth::user()->rol->rol == "Administrador")
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Catalogos</a>
-                            <ul class="dropdown-menu dropdown-menu-dark">
-                                <li><a class="dropdown-item" href="{{url('/users')}}">Usuarios</a></li>
-                            </ul>
-                        </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Catalogos</a>
+                        <ul class="dropdown-menu dropdown-menu-dark">
+                            <li><a class="dropdown-item" href="{{url('/users')}}">Usuarios</a></li>
+                        </ul>
+                    </li>
                     @endif
                     <li class="nav-item dropdown dropdown-dark">
                         <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown">Ingreso de datos</a>
@@ -48,29 +50,29 @@
                             <li><a class="dropdown-item" href="">Solicitudes de sala</a></li>
                         </ul>
                     </li>
-	            </ul>
+                </ul>
                 <ul class="navbar-nav ms-auto">
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->nombres }} ({{Auth::user()->rol->rol}})
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->nombres }} ({{Auth::user()->rol->rol}})
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-end dropdown-menu-dark" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                {{ __('Cerrar sesion') }}
                             </a>
 
-                            <div class="dropdown-menu dropdown-menu-end dropdown-menu-dark" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                    {{ __('Cerrar sesion') }}
-                                </a>
-                                
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
                     @endguest
                 </ul>
             </div>
         </div>
     </nav>
-    
+
     <div class="container my-4">
         @yield('content')
     </div>
@@ -79,4 +81,5 @@
     <script src="http://localhost/sistema_disam/resources/js/menu.js"></script>
     <script src="http://localhost/sistema_disam/resources/js/jquery-3.6.0.min.js"></script>
 </body>
+
 </html>
