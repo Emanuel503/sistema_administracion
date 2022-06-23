c4c5d29d36e0febfa5910f644c9ecfcb07e041e3
Emanuel Molina <emanueljosemolina@hotmail.es>
2022-06-23 12:22:42 -0600

    SolicitudSalaController
    Cambio del nombre del controlador

--- app/Http/Controllers/SolicitudesSalasController.php
+++ app/Http/Controllers/SolicitudesSalasController.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 
-class SalasController extends Controller
+class SolicitudesSalasController extends Controller
 {
     public function comprobarHorario($id_sala, $fecha, $hora_inicio, $hora_finalizacion)
     {
--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UsersController;
-use App\Http\Controllers\SalasController;
+use App\Http\Controllers\SolicitudesSalasController;
 use App\Http\Controllers\ActividadesController;
 
 Auth::routes();
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/actividades/calendario', [App\Http\Controllers\ActividadesController::class, 'calendar']);
-
 Route::resource('/users', UsersController::class)->middleware('auth');
-Route::resource('/solicitudes-sala', SalasController::class)->middleware('auth');
+Route::resource('/solicitudes-sala', SolicitudesSalasController::class)->middleware('auth');
 Route::resource('/actividades', ActividadesController::class)->middleware('auth');
